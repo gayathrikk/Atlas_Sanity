8abaf14261483f4a4e4a524c0890ace3749122be
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-04-09 12:46:01 +0530

    Update Testing.java

--- src/test/java/Sanity/Atlas/Testing.java
+++ src/test/java/Sanity/Atlas/Testing.java
@@ -60,13 +60,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 	        System.out.println("Current URL: " + currentURL);
 			WebDriverWait wait = new WebDriverWait(driver, 10);
 	    	driver.switchTo().defaultContent(); // Switch back to default content
-	    	WebElement viewerElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//img[@src='/viewer/assets/images/colorsvg/gallery.svg']")));
-	    	if (viewerElement.isEnabled() && viewerElement.isDisplayed()) {
-	    	    viewerElement.click();
-	    	    System.out.println("Viewer icon is clicked");
-	    	} else {
-	    	    System.out.println("Viewer icon is not clickable");
-	    	}
+	    	// WebElement viewerElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//img[@src='/viewer/assets/images/colorsvg/gallery.svg']")));
+	    	// if (viewerElement.isEnabled() && viewerElement.isDisplayed()) {
+	    	//     viewerElement.click();
+	    	//     System.out.println("Viewer icon is clicked");
+	    	// } else {
+	    	//     System.out.println("Viewer icon is not clickable");
+	    	// }
 	    	checkConsoleLog();
 	
 	        String parentWindow = driver.getWindowHandle();
